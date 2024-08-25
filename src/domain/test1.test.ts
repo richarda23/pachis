3204c28a84500d59a88e1afa9bad4431e1ea67e8
richarda23 <richard.c.adams@infinityworks.com>
2024-08-25 11:55:23 +0100

    initial commit

---
+++ src/domain/test1.test.ts
@@ -0,0 +1,12 @@
+import { test, expect } from 'vitest';
+
+
+test("is sum", () => {
+    expect(1 + 1).toBe(2);
+    expect(5 + 5).toBe(10);
+})
+
+test("is sum1", () => {
+    expect(1 + 1).toBe(2);
+    expect(5 + 5).toBe(10);
+})
